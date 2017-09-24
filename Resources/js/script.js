9556b484f76f91fa2515be9f9fbceabc5f60a6c7
SanthoshiMadhusudhan <31780631+SanthoshiMadhusudhan@users.noreply.github.com>
2017-09-24 14:43:37 -0700

    updated script.js

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     /* Navigation Scroll */
     
     $(function(){
-        $('a[href*=#]:not([[href*=\\#]])').click(function() {
+        $('a[href*=\\#]:not([href=\\#])').click(function() {
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
                 var target =$(this.hash);
                 target = target.length ? taregt : $('[name=' + this.hash.slice(1) +']');
